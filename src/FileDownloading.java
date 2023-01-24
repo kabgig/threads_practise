e82516886ba4d63bde3933a3ebd3577e349895d4
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-24 15:31:05 +0400

    Just threads with race conditions (should be first commit)

--- src/FileDownloading.java
+++ src/FileDownloading.java
@@ -1,20 +1,16 @@
 public class FileDownloading implements Runnable {
     private DownloadStatus status;
 
-    public FileDownloading() {
-        this.status = new DownloadStatus();
+    public FileDownloading(DownloadStatus status) {
+        this.status = status;
     }
 
     @Override
     public void run() {
-        System.out.println("File downloading " + Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getName() + " started");
         for (int i = 0; i < 10_000; i++) {
-            if (Thread.currentThread().isInterrupted()) return;
             status.incrementTotalBytes();
         }
-    }
 
-    public DownloadStatus getStatus() {
-        return status;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,12 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        var status = new DownloadStatus();
+
         List<Thread> threads = new ArrayList<>();
-        List<FileDownloading> tasks = new ArrayList<>();
 
         for (int i = 0; i < 10; i++) {
-            var task = new FileDownloading();
-            tasks.add(task);
-
-            Thread thread = new Thread(task);
+            Thread thread = new Thread(new FileDownloading(status));
             thread.start();
             threads.add(thread);
         }
@@ -22,9 +20,10 @@ public class Main {
                 throw new RuntimeException(e);
             }
         }
-        var totalBytesSum =     tasks.stream()
-                .map(t -> t.getStatus().getTotalBytes())
-                .reduce(Integer::sum);
-        System.out.println(totalBytesSum);
+
+
+        System.out.println("Total bytes: " + status.getTotalBytes());
+
+
     }
 }
